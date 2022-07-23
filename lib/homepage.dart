169ef84e87c6b2baba645f8a9b864b059c250084
Kittu252001 <saikrishna.alishala@gmail.com>
2022-07-23 22:51:47 +0530

    new changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3919,62 +3919,6 @@ class _HomepageState extends State<Homepage> {
                                 'images/Grilled-chicken.png',
                                 height: 120,
                               ),
-//                                 @override
-//   void initState() {
-//     super.initState();
-//     Timer(Duration(seconds: 3), () {
-//       Navigator.of(context).pop();
-//       Navigator.push(
-//         context,
-//         MaterialPageRoute(
-//           builder: (context) => Homepage(),
-//         ),
-//       );
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         decoration: new BoxDecoration(
-//             gradient: new LinearGradient(
-//           begin: Alignment.topLeft,
-//           end: Alignment.centerRight,
-//           colors: [
-//             const Color(0xFF007991),
-//             const Color(0xFF78ffd6),
-//             // const Color(0xFFFDB99B),
-//           ],
-//         )),
-//         child: Stack(
-//           fit: StackFit.expand,
-//           children: [
-//             Column(
-//               children: [
-//                 Padding(
-//                   padding: EdgeInsets.only(top: 120.0),
-//                 ),
-//                 Text(
-//                   ' Welcome\n To Keus Food Delivery App',
-//                   style: TextStyle(
-//                     fontSize: 30.0,
-//                     fontWeight: FontWeight.bold,
-//                     color: Colors.white,
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: EdgeInsets.only(top: 130.0),
-//                 ),
-//                 LottieBuilder.asset('images/2.json'),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
                               Padding(
                                 padding: const EdgeInsets.only(
                                   right: 40,
